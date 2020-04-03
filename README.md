154f948f99fe7514934dbbcbddb72561a242acce
Edgar Han <edgarh@vt.edu>
2020-04-02 21:24:16 -0400

    added some files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test_dll_imgui_inject
 Test 
+## Working on a test dll injection for directx games
---
+++ test_dll_imgui_inject/dllmain.cpp
@@ -0,0 +1,16 @@
+#include <Windows.h>
+#include <iostream>
+
+#include "dllmain.h"
+
+
+
+
+dllmain::dllmain()
+{
+}
+
+
+dllmain::~dllmain()
+{
+}
